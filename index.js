c8529daeb0e2338b408185bc24743116fadafd8b
Mehdi <mehdi@mehdi-aberkane.fr>
2017-11-10 10:39:56 +0100

    opti code and fixture

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ const fs = require('fs');
 let content = fs.readFileSync('fixtures/user.json');
 var jsonContent = JSON.parse(content);
 
-for (var user in jsonContent) {
-    console.log(user)
-}
-
 app.listen(3000);
 
 app.get('/', function(req, res) {
         res.json(jsonContent);
-    })
-    .post('/', function(req, res) {
-        
     })
 ;
 
